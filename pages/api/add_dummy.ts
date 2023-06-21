b73809b58eb24c9c0acb3fc68717530633b1b57f
Iftekher Toufique Imam <nuhash1083s@gmail.com>
2023-06-21 18:43:07 +0600

    dummy

---
+++ pages/api/add_dummy.ts
@@ -0,0 +1,74 @@
+import clientPromise from "../../lib/mongodb";
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const DBName = "contract_transfer"
+const TransferFromCollection = "transfer_from"
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    const client = await clientPromise;
+    const session = client.startSession();
+    //add dummy data
+    const dummyData = [
+        {
+            "pub_address": "0x08188D0D21fFa2A758Bc8F161e9781f932Af0dAb",
+            "private_key": "3b7f2f5d8544268432152f336698c1852ac47a84e11f9354fd255d7b55b91923"
+        },
+        {
+            "private_key": "081f7309d3573c83419571577e155ba5f6bf3f7a01c2f308e5886505c542c239",
+            "pub_address": "0x11c082BE62016c19a532c207199C6E4a242B06D7",
+        },
+        {
+            "private_key": "257b1355382cdefc49496a30ab14052da22c5184a85777ed7483efe70d3b200b",
+            "pub_address": '0xa9a6B4e1E53BAeea4eD6D6d6341646a23c60fb23'
+        },
+        {
+            "private_key": "cb0aa1f9ec1ab4c18089528f5cb08e7b3a076203cd7760e955372530102cc0c8",
+            "pub_address": '0x8014Eb1616D3608c5C463Bd32f4d1d660D85A762'
+        },
+        {
+            "private_key": "2d8db77dacce4f5298b3f2d4fb9b9159a9547b0decdc705ff6250eca78e27e4e",
+            "pub_address": "0x58D5Be087B3A575f823cb2c38A6b1f7DA8355eD9"
+        },
+        {
+            "private_key": "3894a2b6f23ee3b0cc62c1a2eab5d43dd6d8cd327599d4167fc08fb385e713ae",
+            "pub_address": "0xe0148f6963cec74D5478aBD9c3F326060EB82b02"
+        },
+        {
+            "private_key": "501ed477495cc9eeefaff476c128e756869af2ceb633ed1ef94c3798820e8e0a",
+            "pub_address": "0x8fE7A078ECfE10C42dba6085adB69854Ef98A4Ce",
+        },
+        {
+            "private_key": "55405e25c4d2143355184a53570dbe703eee8aa66eeea21facb2cdde6713d043",
+            "pub_address": '0xd3dd99bB81Bf1D5c26A7b7082dd729281Be558D1',
+        },
+        {
+            "private_key": "bf752758c826dcab6369189c0d19d598832412b11d3167bda4babd0f385da1a8",
+            "pub_address": "0x3Efb17fA71c717738DE40956866b27Fb2A77Ca63",
+        },
+        {
+            "private_key": "3662acc490a441158a9ae575b0c750b7f2058d75ac52a4d5f0b3597f520e6560",
+            "pub_address": "0x1f0F4Bf10769D915AfF39C08a43346D7c472a630",
+        },
+
+    ]
+    try {
+        await session.withTransaction(async () => {
+            const db = client.db(DBName)
+            const transferFromCollection = db.collection(TransferFromCollection)
+            await transferFromCollection.insertMany(dummyData, { session })
+        })
+        res.status(200).json({
+            "success": true
+        })
+
+    } catch (e) {
+        console.error(e)
+        res.status(503).json(e)
+    } finally {
+        session.endSession();
+    }
+}
